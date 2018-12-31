1ccb3c92d1e37267931a9f8d200dc7a9c572490c
JulianCano <juliancano.dev@gmail.com>
2018-12-31 12:03:08 -0500

    Despues del routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,24 @@
-import { NgModule } from '@angular/core';
+// Importar modulos del router de angular
+import { ModuleWithProviders } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+// Importar componentes
+import { HomeComponent } from "./home/home.component";
+import { ZapatillasComponent } from "./zapatillas/zapatillas.component";
+import { VideojuegoComponent } from './videojuego/videojuego.component';
+import { CursosComponent } from './cursos/cursos.component';
 
-@NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
-})
-export class AppRoutingModule { }
+// Array de rutas
+const appRoutes: Routes = [
+  { path: "", component: HomeComponent },
+  { path: "zapatillas", component: ZapatillasComponent },
+  { path: "videojuego", component: VideojuegoComponent },
+  { path: "cursos", component: CursosComponent },
+  { path: '**', component: HomeComponent }
+];
+
+// Exportar el modulo del router
+export const appRoutingProviders: any[] = [];
+export const routing: ModuleWithProviders = RouterModule.forRoot(appRoutes);
+
+
